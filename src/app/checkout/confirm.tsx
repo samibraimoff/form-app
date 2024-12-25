0d27a5f1889a67cee6f4945c89356429c2176bb3
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2024-12-25 00:01:32 -0600

    implement confirm and payment screens using react hook form and zod validation

--- src/app/checkout/confirm.tsx
+++ src/app/checkout/confirm.tsx
@@ -1,7 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
 import CustomButton from "../../components/custom-button";
-import { router } from "expo-router";
+import { Link, router } from "expo-router";
 import KeyboardAwareScrollView from "../../components/keyboard-aware-scroll-view";
+
+const personalInfo = {
+  fullName: "Vadim Savin",
+  address: "Poblenou",
+  city: "Barcelona",
+  postcode: "1234",
+  phone: "60123123123",
+  country: "ES",
+};
+
+const paymentInfo = {
+  cardNumber: "1234123412341234",
+  expires: "01/30",
+  cvv: "123",
+};
 
 export default function Confirm() {
   const onNext = () => {
@@ -14,19 +29,75 @@ export default function Confirm() {
 
   return (
     <KeyboardAwareScrollView>
-      <Text>Confirm submission</Text>
+      <View style={styles.container}>
+        {personalInfo && (
+          <View style={styles.dataContainer}>
+            <View style={styles.dataContainerHeader}>
+              <Text style={styles.title}>Personal</Text>
+              <Link
+                href={"/checkout"}
+                style={{ color: "#005055", fontWeight: "600" }}
+              >
+                Edit
+              </Link>
+            </View>
+            {Object.entries(personalInfo).map(([key, value]) => (
+              <Text key={key}>
+                {key}: {value}
+              </Text>
+            ))}
+          </View>
+        )}
+        {paymentInfo && (
+          <View style={styles.dataContainer}>
+            <View style={styles.dataContainerHeader}>
+              <Text style={styles.title}>Payment</Text>
+              <Link
+                href={"/checkout/payment"}
+                style={{ color: "#005055", fontWeight: "600" }}
+              >
+                Edit
+              </Link>
+            </View>
+            {Object.entries(paymentInfo).map(([key, value]) => (
+              <Text key={key}>
+                {key}: {value}
+              </Text>
+            ))}
+          </View>
+        )}
+      </View>
       <CustomButton onPress={onNext} title={"Submit"} style={styles.button} />
     </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
   button: {
     marginTop: "auto",
-    marginBottom: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 5,
+    paddingBottom: 25,
+    gap: 15,
+  },
+  dataContainer: {
+    borderWidth: 1,
+    borderColor: "gainsboro",
+    padding: 10,
+    borderRadius: 10,
+    gap: 3,
+  },
+  dataContainerHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginBottom: 10,
   },
 });
--- src/app/checkout/payment.tsx
+++ src/app/checkout/payment.tsx
@@ -1,17 +1,62 @@
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import CustomButton from "../../components/custom-button";
+import CustomTextInput from "../../components/custom-text-input";
 import { router } from "expo-router";
 import KeyboardAwareScrollView from "../../components/keyboard-aware-scroll-view";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const PaymentInfoSchema = z.object({
+  cardNumber: z.string({ message: "Card number is required." }).min(1),
+  expiry: z
+    .string()
+    .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, {
+      message: "Date must be in MM/YY format",
+    })
+    .min(1),
+  cvv: z.coerce.number().min(100).max(999),
+});
+
+type PaymentInfo = z.infer<typeof PaymentInfoSchema>;
 
 export default function PaymentDetailsForm() {
-  const onNext = () => {
+  const methods = useForm<PaymentInfo>({
+    resolver: zodResolver(PaymentInfoSchema),
+  });
+  const onNext: SubmitHandler<PaymentInfo> = (data) => {
     router.push("/checkout/confirm");
   };
 
   return (
     <KeyboardAwareScrollView>
-      <Text>Payment</Text>
-      <CustomButton onPress={onNext} title={"Next"} style={styles.button} />
+      <FormProvider {...methods}>
+        <CustomTextInput
+          name="cardNumber"
+          label={"Card number"}
+          placeholder={"1111 2222 3333 4444"}
+        />
+        <View style={{ flexDirection: "row", gap: 5 }}>
+          <CustomTextInput
+            name="expiry"
+            label={"Expiration date"}
+            placeholder={"MM / YY"}
+            containerStyle={{ flex: 1 }}
+          />
+          <CustomTextInput
+            name="cvv"
+            label={"Secure code"}
+            placeholder={"1234"}
+            containerStyle={{ flex: 1 }}
+            inputMode="numeric"
+          />
+        </View>
+        <CustomButton
+          onPress={methods.handleSubmit(onNext)}
+          title={"Next"}
+          style={styles.button}
+        />
+      </FormProvider>
     </KeyboardAwareScrollView>
   );
 }
@@ -23,6 +68,5 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: "auto",
-    marginBottom: 20,
   },
 });
